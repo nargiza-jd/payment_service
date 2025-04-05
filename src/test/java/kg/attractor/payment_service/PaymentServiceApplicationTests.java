b2eced962992fc1c909e11a834a354666f9a730c
nargiza-jd <supabaevanargiza@gmail.com>
2025-04-05 17:11:09 +0600

    Create project

---
+++ src/test/java/kg/attractor/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package kg.attractor.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
